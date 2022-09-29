608bc930c70137b0566e13c1882b88fec0bc63c7
Vincent Bradbury <vincent.bradury223@gmail.com>
2022-09-29 15:06:51 -0500

    update dist with test text

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        <p>A line of text to text redeployment</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
